8b62b2010a11277a97c6fb9fa29f89cb20181497
Wesley Klop <wesley19097@gmail.com>
2017-10-07 16:04:10 +0200

    Fix linting

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import TimePicker from './timepicker';
 
 window.onload = () => {
     const picker = new TimePicker();
-    console.log('test')
 
     picker.bindInput('#bind-one');
     picker.bindInput('#bind-two', {timeFormat: 'military'});
